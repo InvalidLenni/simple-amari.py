01e4fdcc4ecd2c4d6474a4ff96de66e64135d12b
InvalidLenni <68335355+InvalidLenni@users.noreply.github.com>
2022-02-13 14:20:16 +0100

    Add attributes

--- amari/__init__.py
+++ amari/__init__.py
@@ -1,5 +1,9 @@
+# Attribute variables
+__name__ = "simple-amari.py"
 __version__ = "1.0.1"
+__author__ = "InvalidLenni"
 
+## Imports to __init__.py
 from .api import *
 from .exceptions import *
 from .objects import *
